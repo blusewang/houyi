590ec2407c91bbb2ce7c864b8af10bee5bca5175
jf.wang <dev.jf.wang@gmail.com>
2019-07-26 19:57:25 +0800

    update

--- README.md
+++ README.md
@@ -5,21 +5,26 @@ router with middleware
 # example
 
 ```go
-    y := houyi.New()
-	y.NewLayer("a")
-    .Use(func(context *houyi.Context) (err error) {
-		log.Println("middle 1")
-		return 
-	}).Use(func(context *houyi.Context) (err error) {
-		log.Println("middle 2")
-		return
-	}).NewLayer("b").Use(func(context *houyi.Context) (err error) {
-		log.Println("middle 3")
-		return
-	}).Hit("",func(context *houyi.Context) (err error) {
-		log.Println("hit")
-		return
-	})
+	y := houyi.New()
+	y.
+		NewLayer("a").
+		Use(func(context *houyi.Context) (err error) {
+			log.Println("middle 1")
+			return
+		}).
+		Use(func(context *houyi.Context) (err error) {
+			log.Println("middle 2")
+			return
+		}).
+		NewLayer("b").
+		Use(func(context *houyi.Context) (err error) {
+			log.Println("middle 3")
+			return
+		}).
+		Hit("", func(context *houyi.Context) (err error) {
+			log.Println("hit")
+			return
+		})
 
-	log.Println(y.Handle("a.b",[]byte("data")))
+	log.Println(y.Handle("a.b", []byte("data")))
 ```
--- houyi_test.go
+++ houyi_test.go
@@ -1,33 +1,37 @@
 package houyi
 
 import (
-	"errors"
-	"github.com/blusewang/houyi"
 	"log"
 	"testing"
 )
 
 func TestNew(t *testing.T) {
-	y := houyi.New()
-	y.NewLayer("a").Use(func(context *houyi.Context) (err error) {
-		log.Println("middle 1")
-		return errors.New("test err in 1")
-	}).Use(func(context *houyi.Context) (err error) {
-		log.Println("middle 2")
-		return
-	}).NewLayer("b").Use(func(context *houyi.Context) (err error) {
-		log.Println("middle 3")
-		return
-	}).Hit("",func(context *houyi.Context) (err error) {
-		log.Println("hit")
-		return
-	})
+	y := New()
+	y.
+		NewLayer("a").
+		Use(func(context *Context) (err error) {
+			log.Println("middle 1")
+			return
+		}).
+		Use(func(context *Context) (err error) {
+			log.Println("middle 2")
+			return
+		}).
+		NewLayer("b").
+		Use(func(context *Context) (err error) {
+			log.Println("middle 3")
+			return
+		}).
+		Hit("", func(context *Context) (err error) {
+			log.Println("hit")
+			return
+		})
 
-	log.Println(y.Handle("a.b",[]byte("data")))
+	log.Println(y.Handle("a.b", []byte("data")))
 }
 
 func TestArr(t *testing.T) {
-	var a = make(map[string]houyi.HandlersChain)
-	a["sdf"] = houyi.HandlersChain{}
+	var a = make(map[string]HandlersChain)
+	a["sdf"] = HandlersChain{}
 	log.Println(a["sdf"] == nil)
 }
