f556cad84001ebf0647e9039f42036a3b48f3c55
jf.wang <dev.jf.wang@gmail.com>
2019-07-31 19:33:03 +0800

    +logger

---
+++ logger.go
@@ -0,0 +1,21 @@
+package houyi
+
+import (
+	"fmt"
+	"time"
+)
+
+func Logger() HandlerFunc {
+	return func(c *Context) (err error) {
+		start := time.Now()
+
+		// Process request
+		err = c.Next()
+
+		// time cost path err
+		now := time.Now()
+		_, _ = fmt.Fprintf(DefaultErrorWriter,"[HouYi] %v [%v] %v %v\n",now.Format("2006-01-02 15:04:05"),now.Sub(start),c.path,err)
+
+		return
+	}
+}
