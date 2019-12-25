2365df008b374b77b8df403d3aaf4fe05a022783
jf.wang <dev.jf.wang@gmail.com>
2019-12-25 23:38:43 +0800

    update

--- recovery.go
+++ recovery.go
@@ -7,7 +7,7 @@ func Recovery() HandlerFunc {
 		index := context.index
 		defer func() {
 			if err := recover(); err != nil {
-				_, _ = fmt.Fprintf(DefaultErrorWriter, "[panic] [%v] --> %v : %v\n", context.path, nameOfFunction(context.handlers[index]), err)
+				_, _ = fmt.Fprintf(DefaultErrorWriter, "[panic] [%v] --> %v : %v\n", context.uri, nameOfFunction(context.handlers[index]), err)
 			}
 		}()
 		return context.Next()
